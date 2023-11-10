ee213985b7210b5ba12d2756524ce3100f0c785b
Andrey Matveev <andrey.g.matveev@gmail.com>
2023-11-10 23:36:15 +0700

    Revert "feat: add stickers replacement"
    This reverts commit 5a5a9e339b59b1a14c97eb959af5411f4abb6b26.

--- src/constants/replacement-regex.js
+++ src/constants/replacement-regex.js
@@ -2,4 +2,4 @@ import {ORIGIN_REPLACEMENTS_MAP} from "./origin-replacements-map.js";
 
 const originKeysRegex = [...ORIGIN_REPLACEMENTS_MAP.keys()].map((key) => key.replace('.', '\\.')).join('|');
 
-export const ORIGIN_REPLACEMENT_REGEX = new RegExp(`(?<!\\S)(?:https?:\\/\\/)?(?:www\\.)?(${originKeysRegex})\\/(\\S+)`, 'ig');
+export const REPLACEMENT_REGEX = new RegExp(`(?<!\\S)(?:https?:\\/\\/)?(?:www\\.)?(${originKeysRegex})\\/(\\S+)`, 'ig');
